d1081f379de3e4812d13f60ce96227a01267e2f2
Farrukh Khamidov <mrfarrukh.94@gmail.com>
2019-10-19 15:08:09 +0500

    Init commit

---
+++ util/file.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath)
+    fs.unlink(filePath, err => {
+        console.log(err)
+    })
+}
+
+module.exports = clearImage
